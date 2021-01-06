7e06b2d7baace8807d94230d89a4357f6a8b11ca
Hans Olav Sund <hans.olav.sund@digdir.no>
2021-01-06 16:12:18 +0100

    feat: removed all files, deploy handled by helm. First deploy to GCP (#2)
    * feat: removed all files, deploy handled by helm. First deploy to GCP

--- preprocess.sh
+++
@@ -1,3 +0,0 @@
-sed -e 's,${PUBLISHERS_CACHE_USER},'$PUBLISHERS_CACHE_USER',g' \
-  -e 's,${PUBLISHERS_CACHE_PASSWORD},'$PUBLISHERS_CACHE_PASSWORD',g' \
-    </users-template.acl >/usr/local/etc/redis/users.acl
